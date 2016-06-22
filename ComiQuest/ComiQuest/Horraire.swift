e8e8b8d690d800f005eb6f27ccc7bf2a49bbb621
cpagnot3 <clementpagnot@gmail.com>
2016-06-22 17:59:20 +0200

    git end of the day

---
+++ ComiQuest/ComiQuest/Horraire.swift
@@ -0,0 +1,15 @@
+//
+//  Horraire.swift
+//  ComiQuest
+//
+//  Created by Mastere 1 IT on 22/06/2016.
+//  Copyright © 2016 mkdir. All rights reserved.
+//
+
+import Foundation
+
+class Horraire {
+    var startDate : NSDate?
+    var endDate : NSDate?
+    
+}
